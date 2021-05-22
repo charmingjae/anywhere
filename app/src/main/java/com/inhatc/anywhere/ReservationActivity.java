62282c3e554d3c4c8eb8fd3f3bf8e2c446cc2793
wngml317 <wngml317.naver.com>
2021-05-22 20:08:14 +0900

    jh

--- app/src/main/java/com/inhatc/anywhere/ReservationActivity.java
+++ app/src/main/java/com/inhatc/anywhere/ReservationActivity.java
@@ -1,6 +1,8 @@
 package com.inhatc.anywhere;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ReservationActivity extends AppCompatActivity {
 
+    Button doReservation = (Button)findViewById(R.id.btnReservation);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -16,6 +19,17 @@ public class ReservationActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rservation);
+
+        //jh
+        Intent intent = new Intent(this, MyPageActivity.class); // 인텐트를 생성
+        intent.putExtra("bus", "400");   // Intent에 이메일 주소 넣기
+        intent.putExtra("depart", "Seoul");
+        intent.putExtra("arrive", "Seoul");
+        startActivity(intent);
+
+
+
+
 
     }
 }
