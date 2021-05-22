82bdc8354773b31fc1f60b78407006228249cd44
saemmilee <saemmi.lee@gmail.com>
2021-05-23 01:04:10 +0900

    reservation

--- app/src/main/java/com/inhatc/anywhere/MyPageActivity.java
+++ app/src/main/java/com/inhatc/anywhere/MyPageActivity.java
@@ -2,34 +2,34 @@ package com.inhatc.anywhere;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPageActivity extends AppCompatActivity {
-    //jh
-    TextView txtBusNum = (TextView) findViewById(R.id.txtBusNum);
-    TextView txtDepart = (TextView) findViewById(R.id.txtDepart);
-    TextView txtArrive = (TextView) findViewById(R.id.txtArrive);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_form);
 
         Intent intent = getIntent();
+
         //jh
+        TextView txtBus = (TextView) findViewById(R.id.txtBus);
+        TextView txtDepart = (TextView) findViewById(R.id.txtDepart);
+        TextView txtArrive = (TextView) findViewById(R.id.txtArrive);
+
         //ReservationActivity.java에서 보낸 변수로 바꿔주기
         String bus = intent.getStringExtra("bus");
         String depart = intent.getStringExtra("depart");
         String arrive = intent.getStringExtra("arrive");
 
-        txtBusNum.setText(bus);
+        txtBus.setText(bus);
         txtDepart.setText(depart);
         txtArrive.setText(arrive);
     }
--- app/src/main/java/com/inhatc/anywhere/ReservationActivity.java
+++ app/src/main/java/com/inhatc/anywhere/ReservationActivity.java
@@ -14,7 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ReservationActivity extends AppCompatActivity {
 
-    Button doReservation = (Button)findViewById(R.id.btnReservation);
+    Button doReservation;
+
+    //sm
+    String stopData;
+    String busData;
+    String busArrive;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,13 +29,51 @@ public class ReservationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rservation);
 
+        doReservation = (Button)findViewById(R.id.btnReservation);
+
         //sm
         TextView txtBusStop = (TextView)findViewById(R.id.txtBusStop);
-        TextView txtBusNum = (TextView)findViewById(R.id.txtBusNum);
+        TextView txtBusNum = (TextView)findViewById(R.id.txtBus);
+        TextView txtBusArrive = (TextView)findViewById(R.id.txtBusArrive);
+        Button btnReservation = (Button)findViewById(R.id.btnReservation);
+
+        //예약에 필요한 데이터를 intent로 받음
         Intent intent = getIntent();
-        String stopData = intent.getStringExtra("stopname");
-        String busData = intent.getStringExtra("busnum");
+        stopData = intent.getStringExtra("stopname");
+        busData = intent.getStringExtra("busnum");
+
+        //임의의 도착 예정지
+        busArrive = "이태원역";
+        
+        //예약할 정보들을 화면에 띄움
         txtBusStop.setText(stopData);
         txtBusNum.setText(busData);
+        txtBusArrive.setText(busArrive);
+
+        //예약 버튼 클릭 시
+        btnReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*//actibity_reservation_form의 컴포넌트
+                TextView txtBus = (TextView) findViewById(R.id.txtBus);
+                TextView txtDepart = (TextView) findViewById(R.id.txtDepart);
+                TextView txtArrive = (TextView) findViewById(R.id.txtArrive);
+
+                //최종 예약 정보를 화면에 보여줌
+                txtBus.setText(busData);
+                txtDepart.setText(stopData);
+                txtArrive.setText(busArrive);*/
+
+                Intent intent = new Intent(ReservationActivity.this, MyPageActivity.class);
+                intent.putExtra("bus",busData);
+                intent.putExtra("depart",stopData);
+                intent.putExtra("arrive",busArrive);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
     }
 }
